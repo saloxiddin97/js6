b67eba1459a2a79955909903e035c045a626dd75
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-06-07 19:18:48 +0500

    aaaaaaasl

---
+++ script.js
@@ -0,0 +1,22 @@
+let son1 = prompt("son kriting")
+let amal= prompt("1*\n 2/\n3+\n4-")
+let son2 = prompt("ikinchi son kriting")
+
+let natija;
+if(amal==1){
+natija=son1*son2
+}
+ else if(amal==2){
+    natija=son1/son2
+ }
+ else if(amal==3){
+    natija=son1+son2
+ }
+ else if(amal==4){
+    natija=son1-son2
+ }
+ else{
+   alert("xatolik")
+ }
+ alert(natija)
+ 
